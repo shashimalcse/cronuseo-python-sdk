0ec67fc9ff4bf236d84fb60b552836a0e0fe005b
shashimalcse <shashimalsenarath.17@cse.mrt.ac.lk>
2023-04-14 15:58:47 +0530

    fix url

--- cronuseosdk/cronuseo.py
+++ cronuseosdk/cronuseo.py
@@ -9,28 +9,10 @@ class Cronuseo:
         self.__organization = organization
         self.__token = token
 
-    def checkPermission(self,username, permission, resource):
+    def checkPermission(self,username, action, resource):
 
-        url = f"{self.__endpoint}/{self.__organization}/permission/check/username"
-        data = json.dumps({"username": username, "permission": permission, "resource": resource})
-        try: 
-            response = requests.post(url, 
-                                data= data, 
-                                headers={"API_KEY": f"{self.__token}",
-                                        "Content-Type": "application/json"}
-                            )
-            response.raise_for_status()
-            return response.json()
-        except requests.exceptions.RequestException as e:
-            raise SystemExit(e)
-
-    def checkPermissions(self,username, permissions, resource):
-
-        url = f"{self.__endpoint}/{self.__organization}/permission/check/multi_actions"
-
-        permissions = [{"permission": permission} for permission in permissions]
-
-        data = json.dumps({"username": username, "permissions": permissions, "resource": resource})
+        url = f"{self.__endpoint}/{self.__organization}/permission/check"
+        data = json.dumps({"username": username, "action": action, "resource": resource})
         try: 
             response = requests.post(url, 
                                 data= data, 
